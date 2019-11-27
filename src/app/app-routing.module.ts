5bdcb6f3624d82a25ba382c374dbefa75797706b
Sebastian Rios <sebas.asd@hotmail.com>
2019-11-26 23:31:38 -0500

    implementacion de adaptacion grafica componentes y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { WomenComponent } from './women/women.component';
 import { AccesoriesComponent } from './accesories/accesories.component';
 import { HomeComponent } from './home/home.component';
 import { FourZeroFourComponent } from './four-zero-four/four-zero-four.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
@@ -17,6 +19,8 @@ const routes: Routes = [
   {path : 'Men' , component : MenComponent},
   {path : 'Women' , component : WomenComponent},
   {path : 'Accesories' , component : AccesoriesComponent},
+  {path : 'shopingCart' , component : ShoppingCartComponent},
+  {path : 'Login' , component : LoginComponent},
   {path : '**' , component: FourZeroFourComponent}
 
 ];
